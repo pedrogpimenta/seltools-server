6359261111b9e41569a4b71bddd2bbebf4c70b5a
pimentapedro <pedro@meetmaps.com>
2020-10-02 00:07:24 +0200

    Add file creator

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/seltoo
             document.files[file].markers = req.body.files[file].markers || document.files[file].markers
             document.files[file].content = req.body.files[file].content || document.files[file].content
             document.files[file].highlights = req.body.files[file].highlights || document.files[file].highlights
+            document.files[file].creator = req.body.files[file].creator || document.files[file].creator
           }
 
           db.collection('documents').updateOne(
