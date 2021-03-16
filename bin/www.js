c72af240586554a885e4f5f04765ad8757305fbc
pimentapedro <pedro@meetmaps.com>
2021-03-16 13:47:26 +0100

    Add support for new text fields with accepted answers

--- bin/www.js
+++ bin/www.js
@@ -439,6 +439,7 @@ MongoClient.connect(
               document.files[file].markers = req.body.files[file].markers || document.files[file].markers
               document.files[file].content = req.body.files[file].content || document.files[file].content
               document.files[file].highlights = req.body.files[file].highlights || document.files[file].highlights
+              document.files[file].textInputs = req.body.files[file].textInputs || document.files[file].textInputs
               document.files[file].lines = req.body.files[file].lines || document.files[file].lines
               document.files[file].creator = req.body.files[file].creator || document.files[file].creator
               document.files[file].stamps = req.body.files[file].stamps || document.files[file].stamps
